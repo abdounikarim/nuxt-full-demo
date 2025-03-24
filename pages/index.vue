310311a32ce81b80bef2acd534c079f0869522f5
Abdouni Abdelkarim <abdounikarim@gmail.com>
2025-03-24 09:51:10 +0100

    Show books in index

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
 const config = useRuntimeConfig()
 const { data } = await useFetch(config.public.booksApiUrl + '/books');
+let books = [];
+if (data) {
+  books = data.value['hydra:member'];
+}
 </script>
 
 <template>
   <div>
     <Header />
-    <pre>{{ data }}</pre>
+    <div v-if="books">
+      <h2>Books</h2>
+      <ul>
+        <li v-for="book in books" :key="book.book">
+          {{ book.title }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
