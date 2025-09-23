297514be87fb002b1cee813f809b57e306a6d288
Abdouni Abdelkarim <abdounikarim@gmail.com>
2025-09-23 16:34:32 +0200

    Improve code and add reactivity 🚀

--- pages/index.vue
+++ pages/index.vue
@@ -3,18 +3,15 @@ import type { BookJsonld, ApiBooksGetCollection200View } from "~/types/bookJsonl
 const config = useRuntimeConfig()
 const route = useRoute()
 
-let books: BookJsonld[] = [];
-let pagination: ApiBooksGetCollection200View = {};
-let lastPage: number = 1;
-const currentPage = route.query.page ? parseInt(route.query.page as string) : 1
-await useFetch<{'member': BookJsonld[]}>(config.public.bilemoApiUrl + '/books?page=' + currentPage, {
-  method: 'GET',
-  onResponse({ response }) {
-    books = response._data.member;
-    pagination = response._data.view;
-    lastPage = extractPageWithURLSearchParams(pagination.last);
-  },
-})
+const currentPage = computed(() => route.query.page ? parseInt(route.query.page as string) : 1)
+
+const { data, pending, error } = await useFetch<{'member': BookJsonld[], 'view': ApiBooksGetCollection200View}>(
+    () => `${config.public.bilemoApiUrl}/books?page=${currentPage.value}`,
+    {
+      // Key is used to force refresh
+      key: () => `books-page-${currentPage.value}`,
+    }
+)
 
 function extractPageWithURLSearchParams(url: string) {
   try {
@@ -26,17 +23,26 @@ function extractPageWithURLSearchParams(url: string) {
   }
 }
 
+const books = computed<BookJsonld[]>(() => data.value?.member || [])
+const pagination = computed<ApiBooksGetCollection200View>(() => data.value?.view || {})
+const lastPage = extractPageWithURLSearchParams(pagination.value.last);
+
 </script>
 
 <template>
   <div>
     <Header />
     <div>
-      <div v-if="books.length > 0">
+      <div v-if="pending">
+        <p>Pending...</p>
+      </div>
+      <div v-else-if="error">
+        <p>Error while fetching books</p>
+      </div>
+      <div v-else-if="books.length > 0">
         <h2>Books</h2>
         <div v-for="book in books" :key="book.id" class="m-4">
           <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-sm md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-            <!--<img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt="">-->
             <div class="flex flex-col justify-between p-4 leading-normal">
               <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ book.title }}</h5>
               <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ book.id }}</p>
