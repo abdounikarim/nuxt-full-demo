9b475958aea3020b26ae391e22bfdf6501bae31f
Abdouni Abdelkarim <abdounikarim@gmail.com>
2025-09-23 09:35:06 +0200

    Tailwind and Flowbite 🚀

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,16 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  css: ['./assets/css/input.css'], // you'll have to create this file
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 
   modules: [
     '@nuxt/eslint',
